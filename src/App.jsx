fb3b4b87c9e19f25b01fd83dadc2b6711532323f
bingmybongg <luketan945@gmail.com>
2025-05-17 05:50:56 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, this is my first React website!</h1>
+      <button onClick={() => alert("You clicked me!")}>Click Me</button>
+    </div>
+  );
+}
+
+export default App
